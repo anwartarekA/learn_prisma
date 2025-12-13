20e15c2beecb3bdec6d101c26c0caa8732c96a30
= <=>
2025-12-13 02:13:56 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+const adapter = new PrismaPg({
+    connectionString:process.env.DATABASE_URL
+})
+const prisma = new PrismaClient({ adapter })
+const app = express();
+app.use(express.json());
+// create user
+app.post('/', async (req, res) => {
+    const { firstName, lastName, age } = req.body
+    const newUser = await prisma.user.create({
+        data:{firstName, lastName, age}
+    })
+    res.status(201).json(newUser)
+})
+// get all users
+app.get('/', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users)
+})
+// update user
+app.patch('/:id', async (req, res) => {
+    const { age } = req.body;
+    const { id } = req.params;
+    const updatedUser = await prisma.user.update({
+        where: { id: parseInt(id) },
+        data:{age}
+    })
+    res.status(200).json(updatedUser)
+})
+// delete user
+app.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedUser = await prisma.user.delete({
+        where:{id:parseInt(id)}
+    })
+    res.status(200).json(deletedUser)
+})
+const port = 4000;
+app.listen(port, () => console.log(`server is running on port ${port}`))
